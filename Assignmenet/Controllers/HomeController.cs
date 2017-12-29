5c7aa63138081ab38cdc7f3db1bcfe1caa99b2f9
Learning <Learning@DIN86000571.corp.capgemini.com>
2017-12-29 14:18:47 +0530

    Add project files.

---
+++ Assignmenet/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignmenet.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(int? id)
+        {
+            TempData["Id"] = id;
+            return View();
+        }
+
+        public ActionResult Lastpage()
+        {
+
+            TempData["A"] = TempData["Id"];
+            return View();
+        }
+    }
+}
